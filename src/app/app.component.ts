fe736d95012b34ee9aa351b9e4b543f9adaf2273
“Chinmay <chinmay.hardikar.ch@gmail.com>
2022-03-28 18:04:46 +0530

    Inital Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gitpush';
+
+  constructor(){
+    console.log("Added Constructor after making public!!!")
+  }
+
+  numberPrinter(){
+    return 2;
+  }
+}
